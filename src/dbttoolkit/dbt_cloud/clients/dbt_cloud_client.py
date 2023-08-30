0a1cebefc80ee52072f638a93e7e2ba854f2e843
Fernando Brito <fernando.brito@voiapp.io>
2023-08-30 11:10:42 +0200

    fix: handle cancelled jobs (#2)
    * fix: handle cancelled jobs

--- src/dbttoolkit/dbt_cloud/clients/dbt_cloud_client.py
+++ src/dbttoolkit/dbt_cloud/clients/dbt_cloud_client.py
@@ -144,6 +144,7 @@ class DbtCloudClient:
             run
             for run in runs
             if run["is_complete"]
+            and not run["is_cancelled"]
             and run["project_id"] == self.project_id
             and run["environment_id"] == self.environment_id
         ]
